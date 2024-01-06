80b8475fd07e32abb3af3ada59a8754c15576ae9
Park, Sojeong <sojeong2101@gmail.com>
2024-01-06 13:48:24 +0900

    Env: initial setting

---
+++ src/test/java/com/growmming/gurdening/GurdeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growmming.gurdening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GurdeningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
