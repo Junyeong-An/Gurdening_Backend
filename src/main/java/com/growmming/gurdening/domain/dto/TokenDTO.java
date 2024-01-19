8d31ee69c135f9fa5f1a9ca59b62dcb5ad786004
Park, Sojeong <sojeong2101@gmail.com>
2024-01-19 15:45:52 +0900

    :sparkles: Feat: TokenDTO, UserInfoDTO 구현

---
+++ src/main/java/com/growmming/gurdening/domain/dto/TokenDTO.java
@@ -0,0 +1,25 @@
+package com.growmming.gurdening.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+public class TokenDTO {
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    public static class GoogleToken {
+        private String googleAccessToken;
+        private String googleRefreshToken;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    public static class ServiceToken {
+        private String accessToken;
+        private String refreshToken;
+    }
+
+}
