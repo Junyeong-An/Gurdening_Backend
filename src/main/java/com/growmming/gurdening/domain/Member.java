396d0c52881ac84a78fdf74c7880171dc67bd8f8
Park, Sojeong <sojeong2101@gmail.com>
2024-01-19 17:09:35 +0900

    :pencil2: Fix: Member 변수 타입 'l' -> 'L' 로 수정

--- src/main/java/com/growmming/gurdening/domain/Member.java
+++ src/main/java/com/growmming/gurdening/domain/Member.java
@@ -22,7 +22,7 @@ public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
-    private long id;
+    private Long id;
 
     private String name;
 
