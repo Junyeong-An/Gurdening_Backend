df1352ae4b52c00da0e74a17de1b8afbaabe06c0
Park, Sojeong <sojeong2101@gmail.com>
2024-01-24 02:11:29 +0900

    :speech_balloon: add: 빼먹은 어노테이션들 추가

--- src/main/java/com/growmming/gurdening/domain/Bookmark.java
+++ src/main/java/com/growmming/gurdening/domain/Bookmark.java
@@ -1,14 +1,24 @@
 package com.growmming.gurdening.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bookmark extends BaseTimeEntity {
 
     @Id
