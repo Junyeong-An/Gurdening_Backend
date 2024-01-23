7b5375316f0f4c53b13c29dda1e0bd11a8049d01
Park, Sojeong <sojeong2101@gmail.com>
2024-01-23 17:52:57 +0900

    :sparkles: Feat: entity domain 추가

---
+++ src/main/java/com/growmming/gurdening/domain/Image.java
@@ -0,0 +1,20 @@
+package com.growmming.gurdening.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Image extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @Column(name = "image_name", nullable = false)
+    private String imageName;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+}
