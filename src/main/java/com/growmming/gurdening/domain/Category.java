e9c177c76aedce452a26aa3a02dc27bec2216237
Park, Sojeong <sojeong2101@gmail.com>
2024-01-25 01:41:57 +0900

    :art: Modify: Post로 북마크 상태 위치 변경, 카테고리 6개로 변경

--- src/main/java/com/growmming/gurdening/domain/Category.java
+++ src/main/java/com/growmming/gurdening/domain/Category.java
@@ -1,11 +1,10 @@
 package com.growmming.gurdening.domain;
 
 public enum Category {
-    COOKING, // 조리
+    COOKING, // 요리
     DAILY_LIFE, // 일상생활
     CLEANLINESS, // 청결
     TREATMENT, // 치료
     HEALTH, // 건강
     HOME, // 가정(의미가 이상하다 싶으면 Home Economics로 변경 가능)
-    CUISINE // 요리
 }
