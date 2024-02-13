fb116caeb705cb719e5976707f3b4f1f250eff8b
Park, Sojeong <sojeong2101@gmail.com>
2024-02-14 01:47:32 +0900

    :memo: Add: SecurityConfig에 프론트 도메인 추가(cors 문제)

--- src/main/java/com/growmming/gurdening/config/SecurityConfig.java
+++ src/main/java/com/growmming/gurdening/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://gurdening.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Access-Control-Allow-Credentials", "Authorization", "Set-Cookie"));
